9f2a0386727b63a6b2b7aaaeeaa143695a36d9e4
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2018-11-12 00:01:56 +0900

    Update generator

--- generator.rb
+++ generator.rb
@@ -9,7 +9,32 @@ f.each do |line|
 # }
 #
 # EOS
+
+# puts <<~EOS
+# func visit#{klass}(v ast.Visitor, n *ast.#{klass}) interface{} {
+# return nil
+# }
+# 
+# EOS
+
+# ** Node Method
+#   puts <<~EOS
+# func (n *#{klass}) SetParent(parent ast.Node) Node {
+#   n.Parent = parent
+# }
+#
+# EOS
+#   puts <<~EOS
+# func (n *#{klass}) GetType() string {
+#   return "#{klass}"
+# }
+# EOS
+
+
   puts <<~EOS
-Visit#{klass}(Node) interface{}
+func (v *SoqlChecker) Visit#{klass}(n *ast.#{klass}) interface{} {
+return visit#{klass}(v, n)
+}
+
 EOS
 end

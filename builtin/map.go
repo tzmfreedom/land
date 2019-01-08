896520f39f80ba1c71309e83dfcfb682e78b22d5
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2019-01-09 00:12:31 +0900

    Add map

--- builtin/map.go
+++ builtin/map.go
@@ -1,9 +1,51 @@
 package builtin
 
-var MapType = &ClassType{
-	Name: "Map",
+import (
+	"github.com/tzmfreedom/goland/ast"
+)
+
+var MapType = createMapType()
+
+func createMapType() *ClassType {
+	instanceMethods := NewMethodMap()
+	instanceMethods.Set(
+		"put",
+		[]ast.Node{
+			CreateMethod(
+				"add",
+				"",
+				func(this interface{}, params []interface{}, options ...interface{}) interface{} {
+					key := params[0].(*Object).StringValue()
+					thisObj := this.(*Object)
+					values := thisObj.Extra["values"].(map[string]*Object)
+					values[key] = params[1].(*Object)
+					return nil
+				},
+			),
+		},
+	)
+	instanceMethods.Set(
+		"size",
+		[]ast.Node{
+			CreateMethod(
+				"size",
+				"Integer",
+				func(this interface{}, params []interface{}, options ...interface{}) interface{} {
+					thisObj := this.(*Object)
+					return NewInteger(len(thisObj.Extra["values"].(map[string]*Object)))
+				},
+			),
+		},
+	)
+
+	return CreateClass(
+		"Map",
+		[]*ast.ConstructorDeclaration{},
+		instanceMethods,
+		nil,
+	)
 }
 
 func init() {
-	primitiveClassMap.Set("map", MapType)
+	primitiveClassMap.Set("Map", MapType)
 }
